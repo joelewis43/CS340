0761de2ccb21cf0cffdd7e46ce31f54a47f4c9aa
Joseph Lewis <lewisjos@flip3.engr.oregonstate.edu>
2019-02-06 13:38:22 -0800

    added js server

---
+++ helloHandlebars.js
@@ -0,0 +1,28 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 6523);
+
+app.get('/',function(req,res){
+  res.render('home');
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
