512ad5e02c10f7a5ac30457f988cda750ebcfb5d
Joseph Lewis <lewisjos@flip3.engr.oregonstate.edu>
2019-02-06 14:44:55 -0800

    main.handlebars work and cleanup

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
+/******************SETUP******************/
 var express = require('express');
-
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
-
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 6523);
+app.set('port', 7823);
+app.use(express.static(__dirname + '/public'));
+/******************SETUP******************/
 
+
+/**************ROUTE HANDLERS*************/
 app.get('/',function(req,res){
   res.render('home');
 });
 
+app.get('/login',function(req, res){
+  res.render('login');
+});
+
+
+/**************ROUTE HANDLERS*************/
+
+
+/*************ERROR HANDLING**************/
 app.use(function(req,res){
   res.status(404);
   res.render('404');
@@ -22,6 +34,7 @@ app.use(function(err, req, res, next){
   res.status(500);
   res.render('500');
 });
+/*************ERROR HANDLING**************/
 
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
